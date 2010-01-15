496d0b7ae3a0f423ec1d1d97b618bc00a827f121
David Thole <dthole@tdtdev.thedarktrumpet>
2010-01-14 23:07:48 -0600

    Updating tests, adding a launcher

--- test/clwordpresstest.lisp
+++ test/clwordpresstest.lisp
@@ -1,13 +1,20 @@
 ; Tests to test cl-wordpress
 
 (eval-when (:compile-toplevel :load-toplevel :execute)
-  (progn (require :cl-wordpress)
+  (progn (require :asdf)
+	 (require :cl-wordpress)
 	 (require :lift)))
 
 (defpackage :cl-wordpress-test
   (:use :cl :cl-wordpress :lift))
 
 
+; Class Definition Tests - make sure we can access each class, and their respective slots for public access,
+; setting and getting each element.
 
+(lift:deftestsuite wordpress-class-tests ()
+  ())
 
-	 
+(lift:addtest (wordpress-class-tests)
+  eql-test
+  (ensure (eql 1 2)))
---
+++ test/run-tests.lisp
@@ -0,0 +1,12 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (progn (load "clwordpresstest.lisp")))
+
+(in-package :cl-wordpress-test)
+
+(setf *test-describe-if-not-successful?* t)
+(defun run-all-tests ()
+  (format t "Test Output: ~%~a~%" (run-tests)))
+
+; Default is to just run the tests....
+
+(run-all-tests)
